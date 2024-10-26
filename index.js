8fe6c05dbbb384a97a2b12707f5023ec710cf7fb
djewdy <joudi.ammouri10@e-uvt.ro>
2024-10-26 13:46:02 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT;
+const mongoose = require("mongoose")
+const databaseSeeder = require('./databaseSeeder')
+const userRoute = require('./routes/User')
+const productRoute = require('./routes/Product')
+
+
+
+
+mongoose.connect(process.env.mongooseDb).then(()=>console.log("db connceted")).then((err)=>{
+    err;
+})
+
+app.use(express.json());
+
+app.use("/api/seed", databaseSeeder);
+
+app.use("/api/users", userRoute);
+
+app.use("/api/products", productRoute);
+
+
+
+app.listen(PORT || 9000 , ()=>{
+    console.log(`This server is runing on ${PORT}`);
+})
+
+
+//dbusername:joudiammouri
+//dbpassword:Xzk05jWFFtkzGgYX
